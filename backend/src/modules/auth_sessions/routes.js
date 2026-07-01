b8837d45eca370d84bc1b59c8f43c817f3fb0386
Aelberth Cheong <aelberth.cheong@outlook.com>
2026-07-02 00:02:12 +0700

    feat: omnibus commit #3

--- backend/src/modules/auth_sessions/routes.js
+++ backend/src/modules/auth_sessions/routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import requireRateLimit from "../../shared/middlewares/rate_limit.js";
-import requireValidation from "../../shared/middlewares/validation.js";
+import requireRateLimit from "#/shared/middlewares/rate_limit.js";
+import requireValidation from "#/shared/middlewares/validation.js";
 
 import {} from "./controller.js";
 import {} from "./schema.js";
--- backend/src/modules/auth_sessions/service.js
+++ backend/src/modules/auth_sessions/service.js
@@ -1,9 +1,8 @@
-
 export async function createAuthSession() {
 
 }
 
-export async function findAuthSesssion() {
+export async function findAuthSession() {
 
 }
 
